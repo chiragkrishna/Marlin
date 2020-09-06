0042ef058b476d6783699c2d3f3f07e1f151a860
chiragkrishna <chiragkrishna@gmail.com>
2020-09-06 13:47:47 +0530

    Update Auto-build.cmd

--- Auto-build.cmd
+++ Auto-build.cmd
@@ -1,32 +1,4 @@
 @ECHO OFF 
-echo ".................................."
-echo ".................................."
-echo ".................................."
-echo "...Building marlin for Ender 3...."
-echo ".................................."
-echo ".................................."
-echo ".................................."
-git checkout "Ender-3(2.0.x)"
-git pull
-platformio run -e LPC1769
-echo "......................................"
-echo "...build complete for ender 3-2.0.x..."
-echo "......................................"
-copy .pio\build\LPC1769\firmware.bin C:\%homepath%\Desktop\Ender3-2.0.x.bin
-
-git checkout "Ender-3(Bugfix)"
-git pull
-platformio run -e LPC1769
-echo "......................................"
-echo "...build complete for ender 3-Bugfix.."
-echo "......................................"
-copy .pio\build\LPC1769\firmware.bin C:\%homepath%\Desktop\Ender3-Bugfix.bin
-
-echo ""
-echo ""
-echo ""
-echo ""
-echo ""
 
 
 echo ".................................."
@@ -43,14 +15,6 @@ echo "......................................"
 echo "...build complete for ender 5-2.0.x..."
 echo "......................................"
 copy .pio\build\LPC1769\firmware.bin C:\%homepath%\Desktop\Ender5-2.0.x.bin
-
-git checkout "Ender-5(Bugfix)"
-git pull
-platformio run -e LPC1769
-echo "......................................"
-echo "...build complete for ender 3-Bugfix.."
-echo "......................................"
-copy .pio\build\LPC1769\firmware.bin C:\%homepath%\Desktop\Ender5-Bugfix.bin
 
 
 echo firmware ready on desktop
